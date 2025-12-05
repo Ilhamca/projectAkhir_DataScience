78b85e8a7449a92ce706d50b91aa1a89705215d6
Ilhamca <stoneruiner@gmail.com>
2025-12-05 20:32:48 +0700

    Fixed Model Configuration
    Removed Initialize tab setting in order to fix configuration model.

--- app.py
+++ app.py
@@ -19,36 +19,6 @@ def initialize_session_state():
     if "page" not in st.session_state:
         st.session_state.page = "Home" # Default page
 
-def initialize_tab_settings(index):
-    if f"remove_missing_values_checkbox_{index}" not in st.session_state:
-        st.session_state[f"remove_missing_values_checkbox_{index}"] = False
-    if f"remove_duplicates_checkbox_{index}" not in st.session_state:
-        st.session_state[f"remove_duplicates_checkbox_{index}"] = False
-    if f"handle_outliers_checkbox_{index}" not in st.session_state:
-        st.session_state[f"handle_outliers_checkbox_{index}"] = False
-    if f"outlier_method_selectbox_{index}" not in st.session_state:
-        st.session_state[f"outlier_method_selectbox_{index}"] = "IQR Method"
-    if f"normalize_checkbox_{index}" not in st.session_state:
-        st.session_state[f"normalize_checkbox_{index}"] = False
-    if f"normalization_selectbox_{index}" not in st.session_state:
-        st.session_state[f"normalization_selectbox_{index}"] = "Min-Max Scaling"
-    if f"modeling_selectbox_{index}" not in st.session_state:
-        st.session_state[f"modeling_selectbox_{index}"] = "K-Means"
-    if f"n_clusters_{index}" not in st.session_state:
-        st.session_state[f"n_clusters_{index}"] = 3
-    if f"random_state_{index}" not in st.session_state:
-        st.session_state[f"random_state_{index}"] = 42
-    if f"linkage_method_{index}" not in st.session_state:
-        st.session_state[f"linkage_method_{index}"] = "ward"
-    if f"eps_{index}" not in st.session_state:
-        st.session_state[f"eps_{index}"] = 0.5
-    if f"min_samples_{index}" not in st.session_state:
-        st.session_state[f"min_samples_{index}"] = 5
-    if f"n_components_{index}" not in st.session_state:
-        st.session_state[f"n_components_{index}"] = 3
-    if f"covariance_type_{index}" not in st.session_state:
-        st.session_state[f"covariance_type_{index}"] = "full"
-
 # Radio Sync
 def _update_from_primary():
     st.session_state.page = st.session_state.nav_primary
@@ -60,8 +30,6 @@ def _update_from_secondary():
 
 # Initialize session state
 initialize_session_state()
-for i in range(10):
-    initialize_tab_settings(i)
 
 
 
